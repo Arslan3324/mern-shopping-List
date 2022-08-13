159ce37af7473e7e847aa75048820b71d7c36188
Arslan Ahmed <arslanahmed.kcsf18@iba-suk.edu.pk>
2022-08-13 13:09:54 +0500

    backend complete

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://arslan_ahmed:Bhutto123@cluster0.c2kul.mongodb.net/?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+
 const app = express();
+
+
 
 
 //Body Parser middleware
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}
-);
 
-app.listen(4000, () => {
-    console.log('Server started on port 4000');
-});
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+
+// Use Routes
+app.use('/api/items', items);
+
+//port
+const port = process.env.PORT || 5000;
+
+//server listen
+app.listen(port, () => console.log(`Server started on port ${port}`));
